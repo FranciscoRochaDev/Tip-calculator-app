0a5a244da038807a75e28d32f861f2cc8995592b
Francisco Rocha <fa676533@gmail.com>
2026-06-03 00:34:05 -0600

    feat/state de bill, tip y numerooFpeople

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { percentage } from "../data/percentage";
 import FormNumber from "./FormNumber";
 import ResultContainer from "./ResultContainer";
@@ -5,6 +6,11 @@ import Tip from "./Tip";
 
 
 export default function Content(){
+
+    const [bill, setBill] = useState<number | ''>('')
+    const [numberOfPeople, setNumberOfPeople] = useState<number | ''>('')
+    const [customerTip, setCustomerTip] = useState<number | undefined>(undefined)
+
 
     return (
         <div className="bg-white w-full lg:max-w-5xl rounded-t-3xl md:rounded-3xl
@@ -15,23 +21,32 @@ export default function Content(){
                     imgSrc="icon-dollar.svg"
                     label="Bill"
                     placeholder="0"
+                    value={bill}
+                    onChange={setBill}
                 />
                 <div className="flex flex-col gap-2">
-                    <p className="font-space font-bold text-grey-500 text-sm">Select Tip %</p>
+                    <p className="font-space font-bold text-gray-500 text-sm">Select Tip %</p>
                     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                         {percentage.map((tip) => (
                             <Tip
                                 key={tip.percentage}
                                 tip={tip.percentage}
+                                customerTip={customerTip}
+                                setCustomerTip={setCustomerTip}
                             />
                         ))}
-                        <Tip isCustom />
+                        <Tip
+                            isCustom
+                            setCustomerTip={setCustomerTip}
+                        />
                     </div>
                 </div>
                 <FormNumber
                     imgSrc="icon-person.svg"
                     label="Number of People"
                     placeholder="0"
+                    value={numberOfPeople}
+                    onChange={setNumberOfPeople}
                 />
             </div>
 
